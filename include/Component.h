af26fe69ff7580ad9f84dddc5a8658c8bee7fa95
Mathew Rupp <mathewrupp@gmail.com>
2023-04-19 19:13:13 -0700

    worked on project

--- include/Component.h
+++ include/Component.h
@@ -1,12 +1,14 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
-
+#include <string>
 class Component{
   public:
     Component();
     ~Component();
+    std::string getName();
   private:
     int componentType;
+    std::string componentName;
 };
 
 #endif
--- include/Project.h
+++ include/Project.h
@@ -11,7 +11,13 @@ class Project {
 public:
   Project();
   ~Project();
-
+  void setProjectID(int);
+  int getProjectID();
+  void setProjectName(std::string);
+  std::string getProjectName();
+  bool addComponent(Component);
+  void listComponents();
+  bool removeComponent(Component);
 private:
   int projectID;
   std::string projectName;
